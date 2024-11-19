3a92bf625993cf50f8182bac16621fb234c8fb83
gmkoeb <gmkoeb@gmail.com>
2024-11-19 15:59:10 -0300

    Include access to AWS S3 bucket
    - Configured the connection to the AWS S3 bucket.
    
    - Manipulated the request body to convert it into JSON to send to the S3 bucket

---
+++ src/main/java/com/gabriel/createUrlShortener/UrlData.java
@@ -0,0 +1,27 @@
+package com.gabriel.createUrlShortener;
+
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+
+    public UrlData(String originalUrl, long expirationTime) {
+        this.originalUrl = originalUrl;
+        this.expirationTime = expirationTime;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public long getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(long expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+}
